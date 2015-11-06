fdc5f65aa16fbae27adddde9063d3c55febee3f1
mcf171 <453372939@qq.com>
2015-11-06 10:39:04 +0800

    correct the UWiseMethod

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include<algorithm>
 using namespace std;
 
+class Unit;
+
 class Point{
 public:
     int ID;
@@ -15,6 +17,8 @@ public:
         ID = id;
         Data = data;
     }
+    
+    Point(){};
     
     bool operator==(Point &a)
     {
@@ -37,6 +41,7 @@ public:
         Hosts.push_back(host);
         Points = host->Parents;
         std::sort(Points.begin(),Points.end(),SortByID);
+        Count = Points.size();
     }
     
     Unit(Unit* u1, Unit* u2){//u1<=u2
@@ -55,6 +60,11 @@ public:
                     Points.push_back(*it2);
                     it2++;
                 }
+            }else{
+                
+                Points.push_back(*it1);
+                it1++;
+                it2++;
             }
         }
         
@@ -68,6 +78,8 @@ public:
             Points.push_back(*it2);
             it2++;
         }
+        
+        Count = Points.size();
     }
     
     
@@ -86,8 +98,17 @@ public:
         return exits;
     }
     
-    
+    void printUnitGroup(){
+        
+        cout<<" print UnitGroup £∫";
+        for(vector<Point*>::iterator it1  = Points.begin(); it1 != Points.end(); it1 ++)  {
+            
+            cout<< (*it1)->ID<<" ";
+        }
+        cout<<endl;
+    }
 };
+
 
 
 class Node{
@@ -203,38 +224,56 @@ public:
         return 1;
     }
     
-    void Query_GetParents(double* coor1, Point* point){
+    bool Query_GetParents(double* coor1, Point* point){
         int cross=CrossEdge(coor1, point->Data);//0:out;1:cross;2:cover;
         
-        if(cross==0)return;
+        if(cross==0)return false;
         
-        else if(cross==2)Query_InsertAllAsParents(point);
+        else if(cross==2) return Query_InsertAllAsParents(point);
         
         else{
             if(Count>1){
+                bool found = false;
                 for (int i = 0; i < 1<<Dim; i++)
                 {
-                    if(SubNodes[i]!=NULL)SubNodes[i]->Query_GetParents(coor1, point);
+                    if(SubNodes[i]!=NULL){
+                        if(SubNodes[i]->Query_GetParents(coor1, point))found = true;
+                    }
                 }
-            }else if(Count==1){
+                return found;
+            }
+            else if(Count==1)
+            {
                 if(PointInArea(Data, coor1, point->Data)){
                     point->Parents.push_back(Data);
-                    return;
-                }else return;
+                    return true;
+                }
+                else return false;
             }else{
-                return;
+                return false;
             }
         }
     }
     
-    void Query_InsertAllAsParents(Point* point){
+    bool Query_InsertAllAsParents(Point* point){
         if(Count==1){
             point->Parents.push_back(Data);
-        }else if(Count>1){
+            return true;
+        }
+        else if(Count>1)
+        {
+            bool found = false;
             for (int i = 0; i < 1<<Dim; i++)
             {
-                if(SubNodes[i]!=NULL)SubNodes[i]->Query_InsertAllAsParents(point);
+                if(SubNodes[i]!=NULL){
+                    if(SubNodes[i]->Query_InsertAllAsParents(point))found = true;
+                }
             }
+            return found;
+        }
+        else
+        {
+            return false;
         }
     }
     
@@ -298,112 +337,201 @@ public:
     bool Insert(Point* point){
         if (Layers.size() > 0)
         {
-            if (CanDominate(Layers[Layers.size() - 1], point))
-            {
-                if(MAX_LAYER>0 && Layers.size()==MAX_LAYER)return false;
-                
-                Layers.push_back(Node(Dim, Coor1, Coor2));
-                point->Layer = Layers.size() - 1;
-                Layers[point->Layer].Insert(point);
-                
-                Layers[point->Layer].Query_GetParents(Coor1, point);
-                
+            if(MAX_LAYER>0 && Layers.size()==MAX_LAYER){
+                if(CanDominate(Layers[MAX_LAYER - 1], point)) return false;
             }
-            else
+            
+            for (int i = 0; i < Layers.size(); i++)
             {
-                BinarySearchSkyline(0, Layers.size() - 1, point);
+                bool found = Layers[i].Query_GetParents(Coor1, point);
+                if(!found){
+                    point->Layer = i;
+                    Layers[point->Layer].Insert(point);
+                    return true;
+                }
             }
+            
         }
-        else
-        {
-            Layers.push_back(Node(Dim, Coor1, Coor2));
-            point->Layer = Layers.size() - 1;
-            Layers[point->Layer].Insert(point);
-        }
+        
+        Layers.push_back(Node(Dim, Coor1, Coor2));
+        point->Layer = Layers.size() - 1;
+        Layers[point->Layer].Insert(point);
+        
         return true;
-    }
-    
-    void BinarySearchSkyline(int a, int b, Point* point){
-        if (a == b)
-        {
-            point->Layer = a;
-            Layers[a].Insert(point);
-            
-            if(a > 0){
-                Layers[a-1].Query_GetParents(Coor1, point);
-            }
-            
-            return;
-        }
-        else
-        {
-            int i = (a + b) / 2;
-            
-            if (CanDominate(Layers[i], point))
-            {
-                BinarySearchSkyline(i + 1, b, point);
-            }
-            else
-            {
-                BinarySearchSkyline(a, i, point);
-            }
-        }
     }
     
     bool CanDominate(Node layer, Point* data){
         return layer.Query_Bool(Coor1, data->Data);
     }
+    
 };
-
-
 
 void UWiseAddMethod(vector<Point*> AllPoint,int k ){
     
     
-    vector<Unit*> unitGroup;
+    vector<Unit*> unitGroups;
+    vector<Unit*> unitGroupsHighter;
     
     for(vector<Point*>::iterator it  = AllPoint.begin(); it != AllPoint.end(); it ++)  {
         
+        if((*it)->Parents.size() == k){
+            cout<<" G-Skyline £∫";
+            for(vector<Point*>::iterator it1  = (*it)->Parents.begin(); it1 != (*it)->Parents.end(); it1 ++)  {
+                
+                cout<< (*it1)->ID<<" ";
+            }
+            cout<<endl;
+        }
+        
+        if((*it)->Parents.size() < k){
+            Unit* unitGroup = new Unit((*it));
+            unitGroups.push_back(unitGroup);
+        }
+    }
+    bool flag = true;
+    
+    while(flag){
+        vector<Unit*> temp;
+        for(vector<Unit*>::iterator it  = unitGroups.begin(); it != unitGroups.end(); it ++){
+            for(vector<Unit*>::iterator it2  = unitGroups.begin(); it2 != unitGroups.end(); it2 ++)  {
+                
+                Unit* u1 = (*it);
+                Unit* u2 = (*it2);
+                
+                if(u1->Count + u2->Count >= k){
+                    
+                    
+                    Unit* bigUnitGroup = new Unit(u1,u2);
+                    
+                    if(bigUnitGroup->Count == k)
+                        bigUnitGroup->printUnitGroup();
+                    else if(bigUnitGroup->Count <k)
+                        temp.push_back(bigUnitGroup);
+                }
+            }
+            
+        }
+        flag = temp.empty();
+        unitGroupsHighter = temp;
+    }
 }
 
 int main()
 {
-    int SIZE = 10000;
-    int Dim = 8;
-    string PATH = "D:/workspace_cpp/Skyline/datasets/corr_8.txt";
-    int MAX_LAYER = 2;
+    /*
+     int SIZE = 10000;
+     int Dim = 8;
+     string PATH = "F:\\school\\∂¡—–\\«Âª™\\—–“ª\\course\\œ÷¥˙ ˝æ›ø‚∏≈¬€\\ µ—È\\datasets\\test_8.txt";
+     int MAX_LAYER = 2;
+     
+     double** AllData = new double*[SIZE];
+     vector<Point*> AllPoint;
+     
+     
+     ifstream f;
+     f.open(PATH,ios::in);
+     for (int i = 0; i < SIZE; i++)
+     {
+     AllData[i] = new double[Dim];
+     for (int j = 0; j < Dim; j++)
+     {
+     f>>AllData[i][j];
+     }
+     }
+     f.close();
+     
+     double coor1[] = {-500.0,-5.0,-5.0,-5.0,-5.0,-5.0,-5.0,-5.0};
+     double coor2[] = {500.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0};
+     
+     Skylines skylines = Skylines(Dim, MAX_LAYER, coor1, coor2);
+     
+     for (int i = 0; i < SIZE; i++)
+     {
+     Point* point = new Point(i,AllData[i]);
+     if(skylines.Insert(point)){
+     AllPoint.push_back(point);
+     }
+     }
+     */
+    Point* p1 = new Point();
+    p1->ID = 1; 
+    p1->Layer = 1;
     
-    double** AllData = new double*[SIZE];
-    vector<Point*> AllPoint;
+    Point* p2 = new Point();
+    p2->ID = 2; 
+    p2->Layer = 1;
     
+    Point* p3 = new Point();
+    p3->ID = 3; 
+    p3->Layer = 1;
     
-    ifstream f;
-    f.open(PATH,ios::in);
-    for (int i = 0; i < SIZE; i++)
-    {
-        AllData[i] = new double[Dim];
-        for (int j = 0; j < Dim; j++)
-        {
-            f>>AllData[i][j];
-        }
-    }
-    f.close();
+    Point* p4 = new Point();
+    p4->ID = 4; 
+    p4->Layer = 2;
+    p4->Parents.push_back(p2);
     
-    double coor1[] = {-500.0,-5.0,-5.0,-5.0,-5.0,-5.0,-5.0,-5.0};
-    double coor2[] = {500.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0};
+    Point* p5 = new Point();
+    p5->ID = 5; 
+    p5->Layer = 2;
+    p5->Parents.push_back(p3);
     
-    Skylines skylines = Skylines(Dim, MAX_LAYER, coor1, coor2);
+    Point* p6 = new Point();
+    p6->ID = 6; 
+    p6->Layer = 2;
+    p6->Parents.push_back(p3);
     
-    for (int i = 0; i < SIZE; i++)
-    {
-        Point* point = new Point(i,AllData[i]);
-        if(skylines.Insert(point)){
-            AllPoint.push_back(point);
-        }
-    }
+    Point* p7 = new Point();
+    p7->ID = 7; 
+    p7->Layer = 3;
+    p7->Parents.push_back(p4);
+    p7->Parents.push_back(p2);
+    p7->Parents.push_back(p5);
+    p7->Parents.push_back(p3);
     
-    UWiseAddMethod(AllPoint,MAX_LAYER);
+    Point* p8 = new Point();
+    p8->ID = 8; 
+    p8->Layer = 3;
+    p8->Parents.push_back(p2);
+    p8->Parents.push_back(p3);
+    p8->Parents.push_back(p5);
     
+    Point* p9 = new Point();
+    p9->ID = 9; 
+    p9->Layer = 3;
+    p9->Parents.push_back(p6);
+    p9->Parents.push_back(p3);
     
+    Point* p10 = new Point();
+    p10->ID = 10; 
+    p10->Layer = 4;
+    p10->Parents.push_back(p9);
+    p10->Parents.push_back(p8);
+    p10->Parents.push_back(p6);
+    p10->Parents.push_back(p3);
+    p10->Parents.push_back(p2);
+    p10->Parents.push_back(p5);
+    
+    Point* p11 = new Point();
+    p11->ID = 11; 
+    p11->Layer = 4;
+    p11->Parents.push_back(p9);
+    p11->Parents.push_back(p6);
+    p11->Parents.push_back(p3);
+    p11->Parents.push_back(p5);
+    
+    vector<Point*> testPonts;
+    testPonts.push_back(p1);
+    testPonts.push_back(p2);
+    testPonts.push_back(p3);
+    testPonts.push_back(p4);
+    testPonts.push_back(p5);
+    testPonts.push_back(p6);
+    testPonts.push_back(p7);
+    testPonts.push_back(p8);
+    testPonts.push_back(p9);
+    testPonts.push_back(p10);
+    testPonts.push_back(p11);
+    //UWiseAddMethod(testPonts,2);
     return 0;
+    
 }
